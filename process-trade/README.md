1b7ca4934f3cd63a312ddfcc3d7638c6f1cb7619
Michael Ringholm Sundgaard <michaelringholm@gmail.com>
2017-07-12 12:06:54 +0200

    Now working with SNS

--- process-trade/README.md
+++ process-trade/README.md
@@ -16,3 +16,79 @@ https://www.youtube.com/watch?v=G_-aEXmluq8
 
 # Test data
 {"tradeId":"90654","tradeAmount":"20000000", "tradeBook":"FX Spot", "trader":"Michael Sundgaard"}
+
+{
+"default": "{\"tradeId\":\"88654\",\"tradeAmount\":\"20000000\", \"tradeBook\":\"FX Spot\", \"trader\":\"Michael Sundgaard\"}", 
+"lambda": "{\"tradeId\":\"88654\",\"tradeAmount\":\"20000000\", \"tradeBook\":\"FX Spot\", \"trader\":\"Michael Sundgaard\"}"
+}
+
+# Using SNS with Lambda
+https://aws.amazon.com/blogs/mobile/invoking-aws-lambda-functions-via-amazon-sns/
+https://stackoverflow.com/questions/32764138/aws-sns-publishing-to-a-subscribed-lambda-function-logs-null-fields
+
+# Maven sources
+mvn dependency:sources
+
+
+# Large test data (SNS)
+{
+  "Records": [
+    {
+      "EventVersion": "1.0",
+      "EventSubscriptionArn": "arn:aws:sns:EXAMPLE",
+      "EventSource": "aws:sns",
+      "Sns": {
+        "SignatureVersion": "1",
+        "Timestamp": "1970-01-01T00:00:00.000Z",
+        "Signature": "EXAMPLE",
+        "SigningCertUrl": "EXAMPLE",
+        "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
+        "Message": "{\"tradeId\":\"88654\",\"tradeAmount\":\"20000000\", \"tradeBook\":\"FX Spot\", \"trader\":\"Michael Sundgaard\"}",
+        "MessageAttributes": {
+          "Test": {
+            "Type": "String",
+            "Value": "TestString"
+          },
+          "TestBinary": {
+            "Type": "Binary",
+            "Value": "TestBinary"
+          }
+        },
+        "Type": "Notification",
+        "UnsubscribeUrl": "EXAMPLE",
+        "TopicArn": "arn:aws:sns:EXAMPLE",
+        "Subject": "TestInvoke"
+      }
+    }
+  ]
+}
+
+# Large test data 2 (Invalid SNS)
+{
+    "records": [
+        {
+            "sns": {
+                "messageAttributes": {},
+                "signingCertUrl": "https://sns.eu-central-1.amazonaws.com/SimpleNotificationService-b95095beb82e8f6a046b3aafc7f4149a.pem",
+                "messageId": "1feff445-7966-59aa-94d8-a59a8a9d998d",
+                "message": "{\"tradeId\":\"88654\",\"tradeAmount\":\"20000000\", \"tradeBook\":\"FX Spot\", \"trader\":\"Michael Sundgaard\"}",
+                "unsubscribeUrl": "https://sns.eu-central-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:eu-central-1:877257283291:trade-changes:0cf58b55-4df6-4cd7-b2d0-ddaf55b19136",
+                "type": "Notification",
+                "signatureVersion": "1",
+                "signature": "YJfi+u1jiaMveK1iurRmjrbQHsjgbYz7ARqkKsfHfl54ZeqrD81PoMR70z/rr5glpyv08HbJpAUIS7ci7DmBeaeyS4KSF7EPYTpdZdzkrrJrvP+ozc4xxy0Lv6250zggB8LaUTXE9NqRecQlh/mRYdYWBq6964/enXVyac3Wuno1SjTXHrYpQ/uKnbGDK9x01q6YuBJQ9hBeJdq/7Hqwl7qIZJq6yzYK16L8BVV63fjk4OO3vZlGFK/tvReC1MKxnbl5KrvpZ9W0Eb7bcAvmgeykFz6owpkwjdk0NRKPb5idy9LWVlP/NjTYqYG7qYLapy5bXHqK2LmnuD7eYmY2rw==",
+                "timestamp": {
+                    "iMillis": 1499853243748,
+                    "iChronology": {
+                        "iBase": {
+                            "iMinDaysInFirstWeek": 4
+                        }
+                    }
+                },
+                "topicArn": "arn:aws:sns:eu-central-1:877257283291:trade-changes"
+            },
+            "eventVersion": "1.0",
+            "eventSource": "aws:sns",
+            "eventSubscriptionArn": "arn:aws:sns:eu-central-1:877257283291:trade-changes:0cf58b55-4df6-4cd7-b2d0-ddaf55b19136"
+        }
+    ]
+}
--- process-trade/src/main/java/com/stelinno/aws/lambda/ProcessTrade.java
+++ process-trade/src/main/java/com/stelinno/aws/lambda/ProcessTrade.java
@@ -1,5 +1,7 @@
 package com.stelinno.aws.lambda;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,10 +13,16 @@ import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.model.TableDescription;
 import com.amazonaws.services.lambda.runtime.Context; 
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent.SNSRecord;
+import com.amazonaws.services.s3.event.S3EventNotification;
+import com.amazonaws.services.sns.model.SubscribeResult;
 import com.google.gson.Gson;
 
 //public class ProcessTrade implements RequestHandler<Integer, String> {
-public class ProcessTrade implements RequestHandler<Trade, String> {
+//public class ProcessTrade implements RequestHandler<Trade, String> {
+//public class ProcessTrade implements RequestHandler<Object, String> {
+public class ProcessTrade implements RequestHandler<SNSEvent, String> {
 	static final Logger logger = Logger.getLogger(ProcessTrade.class.getName());
 	static final Gson gson = new Gson();
 	
@@ -26,50 +34,124 @@ public class ProcessTrade implements RequestHandler<Trade, String> {
 		return String.format("Processed trade with id %d via handleRequest method", tradeId);
 	}*/
 	
-	public String handleRequest(Trade trade, Context context) {
+	/*public String handleRequest(SNSEvent event, Context context) {
+		logger.info("*************** SNSEvent ****************");
+		logger.info(gson.toJson(event));		
+		logger.info("*********** END OF SNSEvent *************");
+		logger.info("*************** Message ****************");
+		logger.info(event.getRecords().get(0).getSNS().getMessage());
+		logger.info("*********** END OF Message *************");
+		return "got an event!";
+	}*/
+	
+	/*public String handleRequest(S3EventNotification eventNotofication, Context context) {
+		logger.info(String.format("got %d records", eventNotofication.getRecords().size()));
+		logger.info("*************** JSON DATA ****************");
+		logger.info(eventNotofication.toJson());
+		eventNotofication.
+		logger.info("*********** END OF JSON DATA *************");
+		return "got an event!";
+	}*/
+	
+	/*public String handleRequest(Object trade, Context context) {
+		if(trade != null) {
+			logger.info("*************** DATA ****************");
+			logger.info(trade.toString());
+			logger.info("*********** END OF DATA *************");
+			logger.info("CLASS = " + trade.getClass().getName());
+			
+			try {
+				logger.info("Trying to cast object to SubscribeResult...");
+				SubscribeResult result = (SubscribeResult)trade;
+				logger.info("Cast object succeeded!");
+			}
+			catch(Exception e) {
+				logger.log(Level.SEVERE, e.getMessage());
+			}
+			
+			try {
+				logger.info("Trying to cast object to S3EventNotification...");
+				S3EventNotification result = (S3EventNotification)trade;
+				logger.info("Cast object succeeded!");
+			}
+			catch(Exception e) {
+				logger.log(Level.SEVERE, e.getMessage());
+			}
+			
+			try {
+				logger.info("Trying to parse JSON to S3EventNotification...");
+				S3EventNotification result = S3EventNotification.parseJson(trade.toString());
+				logger.info("Parse object succeeded!");
+			}
+			catch(Exception e) {
+				logger.log(Level.SEVERE, e.getMessage());
+			}	
+			
+			try {
+				logger.info("Trying via LinkedHashMap ...");
+				HashMap<String, Object> map = (LinkedHashMap)trade;
+				logger.info("Parse LinkedHashMap succeeded!");
+			}
+			catch(Exception e) {
+				logger.log(Level.SEVERE, e.getMessage());
+			}				
+		}
+		else
+			logger.info("object was null");
 		
+		return "fully done!";
+	}*/
+	
+	public String handleRequest(SNSEvent event, Context context) {
 		logger.info("handle request is starting....");
-		/*AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
-		DynamoDB dynamoDB = new DynamoDB(client);
-
-		Table table = dynamoDB.getTable("Trade");
-		Item tradeItem = table.getItem("Id", trade.getTradeId());
-		table.putItem(tradeItem);*/
 		
-		// Must allow the policy in IAM inline policy for this to work
-		final AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.defaultClient();
-		logger.info("ddb created....");
-		
-		try {
-			DynamoDB dynamoDB = new DynamoDB(ddb);
-			logger.info("dynamoDB created....");
+		Iterator<SNSRecord> recordsIter = event.getRecords().iterator();
+		while(recordsIter.hasNext()) {
+			SNSRecord snsRecord = recordsIter.next();
+			Trade trade = gson.fromJson(snsRecord.getSNS().getMessage(), Trade.class);
+			/*AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+			DynamoDB dynamoDB = new DynamoDB(client);
+	
 			Table table = dynamoDB.getTable("Trade");
-			logger.info("got table....");
+			Item tradeItem = table.getItem("Id", trade.getTradeId());
+			table.putItem(tradeItem);*/
 			
-			//TableDescription desc = table.describe();
-			//logger.info("described table....");
+			// Must allow the policy in IAM inline policy for this to work
+			final AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.defaultClient();
+			logger.info("ddb created....");
 			
-			//table.getItem("tradeId", 10987);
-			
-			//Item tradeItem = table.getItem("Id", trade.getTradeId());
-			Item tradeItem = new Item();
-			tradeItem.withPrimaryKey("tradeId", trade.getTradeId());
-			tradeItem.withDouble("tradeAmount", trade.getTradeAmount());
-			tradeItem.withString("tradeBook", trade.getTradeBook());
-			tradeItem.withString("trader", trade.getTrader());
-			tradeItem.withJSON("trade", gson.toJson(trade));
-			
-			logger.info("created item....");
-			table.putItem(tradeItem);
-			logger.info("putting item....");
-						
+			try {
+				DynamoDB dynamoDB = new DynamoDB(ddb);
+				logger.info("dynamoDB created....");
+				Table table = dynamoDB.getTable("Trade");
+				logger.info("got table....");
+				
+				//TableDescription desc = table.describe();
+				//logger.info("described table....");
+				
+				//table.getItem("tradeId", 10987);
+				
+				//Item tradeItem = table.getItem("Id", trade.getTradeId());
+				Item tradeItem = new Item();
+				tradeItem.withPrimaryKey("tradeId", trade.getTradeId());
+				tradeItem.withDouble("tradeAmount", trade.getTradeAmount());
+				tradeItem.withString("tradeBook", trade.getTradeBook());
+				tradeItem.withString("trader", trade.getTrader());
+				tradeItem.withJSON("trade", gson.toJson(trade));
+				
+				logger.info("created item....");
+				table.putItem(tradeItem);
+				logger.info("putting item....");
+							
+			}
+			catch(Throwable e) {
+				logger.log(Level.SEVERE, e.getMessage(), e);
+			}
+			finally {
+				//context.getClientContext().
+			}
 		}
-		catch(Throwable e) {
-			logger.log(Level.SEVERE, e.getMessage(), e);
-		}
-		finally {
-			//context.getClientContext().
-		}
+		
 		return String.format("Processed POJO trade with id %d and amount %s via handleRequest method V2", 
 				trade.getTradeId(), String.valueOf(trade.getTradeAmount()));
 	}
